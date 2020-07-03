2c03e4fbe450363e968368ffa96bf25cabc5235f
bcjrsnappymob <brian@snappymob.com>
2020-07-03 14:02:31 +0800

    Finished until Create a Feature Component from the AngularJS Tour of Heroes

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+
+// Decorator function that specifies the Angular metadata for the component
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+  constructor() { }
+
+  // A lifecycle hook for the component
+  ngOnInit(): void {
+
+  }
+
+}
