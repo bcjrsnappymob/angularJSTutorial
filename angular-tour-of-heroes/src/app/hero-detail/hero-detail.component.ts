a6c911626a58986ffa21ce43cd700d5872cc2fb7
bcjrsnappymob <brian@snappymob.com>
2020-07-06 12:09:29 +0800

    Finished Tutorial

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,40 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { HeroService } from '../hero.service';
+
+import { Hero }         from '../hero';
+import { HeroService }  from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-
   @Input() hero: Hero;
 
   constructor(
-    // Extract route parameter from URL with id
     private route: ActivatedRoute,
-    // Gets hero data from the remote server and get the hero-to-display
-    private HeroService: HeroService,
-    // To navigate back to the view that navigated here
+    private heroService: HeroService,
     private location: Location
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
   }
 
-  /*
-    route.snapshot: A static image of the route information after the component was created.
-    paramMap: A dictionary of route parameter values extracted from the URL.
-    + operator: Converts the string to a number.
-  */
-
-  getHero(): void{
+  getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.HeroService.getHero(id)
+    this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
@@ -42,4 +33,8 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
