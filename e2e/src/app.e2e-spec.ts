daf6476fc96b10983b303967b03ad757cbeebfcd
bcjrsnappymob <brian@snappymob.com>
2020-07-22 11:02:37 +0800

    Added e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,21 +1,53 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Tour of Heroes App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    browser.get('https://tourofheroesangular.netlify.app/dashboard');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toContain('Tour of Heroes');
+  it('dashboard should not be empty', () => {
+    expect(element(by.className('module hero')).getText()).not.toBeUndefined();
   });
 
-  it('should display subWelcome message', () => {
-    page.navigateTo();
-    expect(page.getSubTitleText()).toContain('Angular');
+  it('should have a page title', () => {
+    expect(element(by.css('h1')).getText()).toMatch('Tour of Heroes');
+  });
+
+  it('should have a dashboard title', () => {
+    expect(element(by.css('h3')).getText()).toMatch('Top Heroes');
+  });
+
+  it('should navigate to heroes page', () => {
+    const heroesButton = element.all(by.css('nav a')).last();
+    expect(heroesButton.getText()).toContain('Heroes');
+    if (heroesButton.click()){
+      browser.get('https://tourofheroesangular.netlify.app/heroes');
+      expect(element(by.css('li')).getText()).not.toBeUndefined();
+    }
+  });
+
+  it('should navigate to dashboard from heroes page', () => {
+    browser.get('https://tourofheroesangular.netlify.app/heroes');
+    expect(element(by.css('li')).getText()).not.toBeUndefined();
+    const dashboardButton = element.all(by.css('nav a')).first();
+    expect(dashboardButton.getText()).toContain('Dashboard');
+    if (dashboardButton.click()){
+      browser.get('https://tourofheroesangular.netlify.app/dashboard');
+      expect(element(by.css('h3')).getText()).toMatch('Top Heroes');
+    }
+  });
+
+  it('should navigate to hero details page', () => {
+    const heroDetailsButton = element.all(by.css('.heroes li a'));
+    expect(heroDetailsButton.getText()).not.toBeUndefined();
+    if (heroDetailsButton.click()){
+      browser.get('https://tourofheroesangular.netlify.app/detail/11');
+      expect(element(by.css('button')).getText()).toContain('go back');
+    }
   });
 
   afterEach(async () => {
